52688043c23f521e41be4959358ba9e38b6e129b
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2023-07-29 11:15:12 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "purchase_reports_improvements",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "category": "Purchase",
+    "data": [
+        'purchase_report.xml',
+    ],
+}
